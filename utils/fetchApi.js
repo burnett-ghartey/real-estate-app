37036153a3bf4a8de4a718a4b186f47ff20a9b2f
Burnett <00burnettghartey@gmail.com>
2023-02-01 10:32:17 +0000

    add node modules to gitignore

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = "https://bayut.p.rapidapi.com"
 export const fetchApi = async(url) => {
     const {data} = await axios((url), {
         headers: {
-    'X-RapidAPI-Key': 'f0322fe01bmshf3e0fb6a211f266p1bc484jsn25fc6ab2a181',
+    'X-RapidAPI-Key': process.env.RAPID_API_KEY,
     'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
   }
     })
